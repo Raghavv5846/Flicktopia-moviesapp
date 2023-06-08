690d5a53c8a8f897faa3cb6cf56fcf35f97fc53e
raghavv5846 <raghav.pareek5846@gmail.com>
2023-06-08 20:11:55 +0530

    commit

---
+++ config/passport_local.js
@@ -0,0 +1,66 @@
+const passport=require('passport');
+const LocalStrategy=require('passport-local');
+const User=require('../models/user');
+passport.use(new LocalStrategy({
+    usernameField:'email',
+    passReqToCallback: true
+},
+    function(req, email,password,done){
+        let user = User.findOne({email: email})
+        .then((user)=>{
+            
+            // Check if the password matches the user's password.
+            if (!user || user.password !== password) {
+                // Password does not match.
+                console.log('Password mismatch');
+                return done(null, false);
+            }
+            // Password matches.
+            console.log('Login successful');
+            
+            return done(null, user);
+        })
+        .catch((err)=>{
+            console.log(err);
+            return done(err);
+        });
+
+        
+        }
+    ));
+// serializing the user to decide which key is to kept in the cookie
+passport.serializeUser(function(user,done){
+    console.log(user);
+    done(null,user.id);
+});
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function(id,done){
+    User.findById(id)
+    .then((user)=>{
+        if(user){
+            return done(null,user);
+        }
+    }).catch((err)=>{
+        if(err){
+            console.log("err in finding user id through passport",err);
+        }
+    })
+});
+// check i fth euser is signed in
+passport.checkAuthentication=function(req,res,next){
+    // if th euser is signed in then pass the request to the next function(controller action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+    // if the user is not signed in
+    console.log(req.sessionID);
+    return res.send({status:"failed"});
+}
+passport.setAuthenticatedUser = function(req, res, next){
+    if (req.isAuthenticated()){
+        // req.user contains the current signed in user from the session cookie and we are just sending this to the locals for the views
+        res.locals.user = req.user;
+    }
+    next();
+}
+module.exports=passport;
---
+++ index.js
@@ -0,0 +1,80 @@
+const express=require('express');
+const cookieParser=require('cookie-parser');
+const app=express();
+const db=require('./config/mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport_local');
+const MongoStore = require('connect-mongo');
+let User=require('./models/user');
+const cors=require('cors');
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+    app.use(cookieParser());
+
+    app.use(session({
+        name:'flicktopia',
+        // todo change the secret before deployment in production mode
+        secret:'blahsomething',
+        saveUninitialized:false,
+        resave:false,
+        cookie:{
+            maxAge:(1000*30*10)
+        },
+        store: MongoStore.create(
+            {
+                mongoUrl: `mongodb://127.0.0.1:27017/flicktopia`,
+                autoRemove: 'disabled'
+            
+            },
+            function(err){
+                console.log(err ||  'connect-mongodb setup ok');
+            }
+        )
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(passport.setAuthenticatedUser);
+
+app.get('/protected',passport.checkAuthentication,async (req,res)=>{
+    console.log(res.locals.user);
+
+    return res.send({user:res.locals.user.name,status:"success"})
+})
+
+app.post('/sign-in/create',async (req,res)=>{
+    console.log(req.body);
+    if (req.body.password != req.body.confirm_password){
+        console.log("wrong pass");
+        return res.status(404).send('wrong');
+        return res.redirect('back');
+    }
+
+    let user= await User.findOne({email: req.body.email})
+        if (!user){
+            await User.create(req.body);
+            console.log("user created succesfyllu");
+            return res.status(200).send({status:"success"});
+        return res.redirect('back');
+
+        }else{
+            console.log("users already registred");
+            return res.status(400).send('wrong');
+        return res.redirect('http://localhost:3000/');
+
+        }
+});
+app.post('/sign-in/create-session',passport.authenticate(
+    'local',
+    {failureRedirect: 'http://localhost:3000/sign-up'},
+), (req,res)=>{
+    console.log("logged in successfully",req.sessionID);
+    res.status(200).send({status:"success"});
+});
+
+app.listen(8000,(err)=>{
+    if(err){
+        console.log('Error in Listening on port 8000',err);
+    }
+    console.log("succesfully connected to port 8000");
+})
