2fea0d2837dcf45f1ee9105e7127430a4e08fdad
Nikhil Gupta <Gup7nik@gmail.com>
2023-10-11 00:20:29 +0530

    sas

---
+++ server/config/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+main().catch(err => console.log(err));
+
+async function main() {
+    console.log("mongoose has connected");
+    
+  await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@flicktopia.np3wnm1.mongodb.net/?retryWrites=true&w=majority`);
+}
