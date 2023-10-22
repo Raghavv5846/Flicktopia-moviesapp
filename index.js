ff3dba04341c80638bd2a419e88cd8f0a154605a
Raghavv5846 <89181921+Raghavv5846@users.noreply.github.com>
2023-10-22 19:23:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.use(cookieParser());
         cookie:{
             maxAge:(1000*30*10*10),
             secure:true,
+            sameSite:none,
         },
         store: MongoStore.create(
             {
